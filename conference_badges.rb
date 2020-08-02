81bd6e2363dc147166d1de5ce39d8a82391a8547
Brian Donnoe <dunow33@hotmail.com>
2020-08-02 17:58:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,10 +25,10 @@ def assign_rooms(speakers)
 end 
 
 def printer(attendees)
-  batch_badge_creator(nameArray).each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts #{badge}
   end
-  assign_rooms(speakerArray).each do |speaker|
+  assign_rooms(attendees).each do |speaker|
     puts #{speaker}
   end
 end
