1be45bacd5c7e65f48b4b479fe12bca4293bbfb1
Brian Donnoe <dunow33@hotmail.com>
2020-08-02 17:39:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages.push("Hello, my name is #{name}.")
+  end
+  return badge_messages
+end
+
+def assign_rooms(speakers)
+  rooms = [7]
+  speakerList = []
+  counter = 1
+  speakers.each do |speaker|
+    speakerList.push("Hello, #{speaker}! You'll be assigned to room #{counter}!")
+    rooms.push(speaker)
+    counter += 1
+  end
+  return speakerList
+end 
+
+def printer
+  batch_badge_creator
+  puts assign_rooms
+end
